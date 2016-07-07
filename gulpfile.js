34b3b836ee5a173f969367639aa8733ad8a9f3bc
limzykenneth <limzy.kenneth@gmail.com>
2016-07-07 01:15:43 +0100

    Add copy of lib

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var insert = require('gulp-insert');
 var rename = require("gulp-rename");
 var rjs = require('gulp-requirejs');
+var yuidoc = require("gulp-yuidoc");
 
 gulp.task('default', function() {
   return gulp.src('data.json')
@@ -24,5 +25,24 @@ gulp.task('requirejs', function() {
         'jquery': 'empty:'
     }
   })
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('yuidoc', function(){
+  gulp.src(['./src/lib', './src/lib/addons/'])
+    .pipe(yuidoc({
+      project: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          themedir: './src/yuidoc-p5-theme/',
+          helpers: ['./src/yuidoc-p5-theme/helpers/helpers.js'],
+          preprocessor: './src/preprocessor.js',
+          outdir: './dist/reference/'
+        }
+      }
+    }))
     .pipe(gulp.dest('./'));
 });
--- src/yuidoc-p5-theme/helpers/helpers.js
+++ src/yuidoc-p5-theme/helpers/helpers.js
@@ -1,9 +1,19 @@
+var configHelpers = {};
+
+// For production, the reference docs are put in the /reference/ folder
+// of the p5 website, so we'll define our paths with this assumption.
+// For more context, see https://github.com/processing/p5.js-website.
 var config = {
   p5SiteRoot: './reference',
   p5Lib: './reference/lib/p5.min.js',
   p5SoundLib: './reference/lib/addons/p5.sound.min.js',
-  p5DomLib: './reference/lib/addons/p5.dom.min.js',
-  projectAssets: './reference/assets'
+  p5DomLib: './reference/lib/addons/p5.dom.min.js'
 };
 
-module.exports = config;
+Object.keys(config).forEach(function(key) {
+  configHelpers[key] = function() {
+    return config[key];
+  };
+});
+
+module.exports = configHelpers;

0728b8fb7b534494f64000f64a93fe5d6dc7c437
limzykenneth <limzy.kenneth@gmail.com>
2016-07-07 01:04:46 +0100

    We need to build from source

---
+++ src/yuidoc-p5-theme/helpers/helpers.js
@@ -0,0 +1,9 @@
+var config = {
+  p5SiteRoot: './reference',
+  p5Lib: './reference/lib/p5.min.js',
+  p5SoundLib: './reference/lib/addons/p5.sound.min.js',
+  p5DomLib: './reference/lib/addons/p5.dom.min.js',
+  projectAssets: './reference/assets'
+};
+
+module.exports = config;
